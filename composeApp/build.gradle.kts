cbefc3d3372a29057d2f83f05743ee05a9b2ed8f
WooJin Kong <kongwoojin03@gmail.com>
2025-09-17 10:00:49 +0900

    del: Remove duplicated config

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -27,28 +27,17 @@ kotlin {
 
 android {
     namespace = "in.koreatech.business"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "in.koreatech.business"
-        minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
     }
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
     }
 }
 
