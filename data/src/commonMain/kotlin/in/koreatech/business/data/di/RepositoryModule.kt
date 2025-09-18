64d8bd96021477244125c683708e25ea3adfefba
WooJin Kong <kongwoojin03@gmail.com>
2025-09-18 21:17:44 +0900

    add: Add OwnerRepository

--- data/src/commonMain/kotlin/in/koreatech/business/data/di/RepositoryModule.kt
+++ data/src/commonMain/kotlin/in/koreatech/business/data/di/RepositoryModule.kt
@@ -1,6 +1,8 @@
 package `in`.koreatech.business.data.di
 
+import `in`.koreatech.business.data.repository.OwnerRepositoryImpl
 import `in`.koreatech.business.data.repository.TokenRepositoryImpl
+import `in`.koreatech.business.domain.repository.OwnerRepository
 import `in`.koreatech.business.domain.repository.TokenRepository
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
@@ -8,5 +10,6 @@ import org.koin.dsl.module
 
 val repositoryModule =
     module {
+        singleOf(::OwnerRepositoryImpl) { bind<OwnerRepository>() }
         singleOf(::TokenRepositoryImpl) { bind<TokenRepository>() }
     }
---
+++ domain/src/commonMain/kotlin/in/koreatech/business/domain/repository/OwnerRepository.kt
@@ -0,0 +1,3 @@
+package `in`.koreatech.business.domain.repository
+
+interface OwnerRepository
