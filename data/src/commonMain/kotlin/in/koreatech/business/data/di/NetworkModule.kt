ed03ab4e66230a0ef8c86c0d1b614e2ec714ba7e
WooJin Kong <kongwoojin03@gmail.com>
2025-09-18 21:17:00 +0900

    add: Add OwnerApi and OwnerAuthApi

--- data/src/commonMain/kotlin/in/koreatech/business/data/di/NetworkModule.kt
+++ data/src/commonMain/kotlin/in/koreatech/business/data/di/NetworkModule.kt
@@ -1,5 +1,7 @@
 package `in`.koreatech.business.data.di
 
+import `in`.koreatech.business.data.api.OwnerApi
+import `in`.koreatech.business.data.api.auth.OwnerAuthApi
 import `in`.koreatech.business.data.network.provideAuthHttpClient
 import `in`.koreatech.business.data.network.provideNoAuthHttpClient
 import org.koin.core.module.dsl.singleOf
@@ -8,6 +10,9 @@ import org.koin.dsl.module
 
 val networkModule =
     module {
+        singleOf(::OwnerApi) { named("noAuth") }
+        singleOf(::OwnerAuthApi) { named("auth") }
+
         singleOf(::provideAuthHttpClient) { named("auth") }
         singleOf(::provideNoAuthHttpClient) { named("noAuth") }
     }
